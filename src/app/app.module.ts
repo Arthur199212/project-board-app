22629f3a662f24a319ba5c5e3f76ecde6b9b3ee3
Arthur Rubchenko <2146596@mail.ru>
2019-09-25 00:35:27 +0200

    feat: add markup of a toolbar & a little logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,10 +27,13 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  @Output() public childEvent = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public searchCriterion: string = '';
+
+  handleClick() {
+    this.childEvent.emit(this.searchCriterion);
+  }
+
+  onKeydown({ key }) {
+    if (key === "Enter") {
+      this.handleClick();
+    }
+  }
 }
