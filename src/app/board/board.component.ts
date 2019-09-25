57cf976f1bb5d10e37b85378554002febb3be457
Arthur Rubchenko <2146596@mail.ru>
2019-09-26 00:57:58 +0200

    feat: add board markup & basic logic

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  selector: "app-board",
+  templateUrl: "./board.component.html",
+  styleUrls: ["./board.component.scss"]
 })
 export class BoardComponent implements OnInit {
+  cardList = [
+    {
+      id: "1",
+      name: "Backlog",
+      cards: []
+    },
+    {
+      id: "2",
+      name: "In progress",
+      cards: []
+    },
+    {
+      id: "3",
+      name: "Done",
+      cards: []
+    }
+  ];
 
-  constructor() { }
+  @Input() public searchCriterion;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  removeCard({ cardId }) {
+    this.cardList = this.cardList.filter(({ id }) => id !== cardId);
   }
-
 }
